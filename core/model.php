3a3f2fd8e3e395da9761c45c6d3c9a43b373ef6b
Hossein Beiki <ho33ein.b@gmail.com>
2024-05-14 19:01:29 +0330

    Fix issue

--- core/model.php
+++ core/model.php
@@ -3,7 +3,7 @@ require_once "public-function.php";
 
 class Model
 {
-    public static $db = '';
+    public static $conn = '';
 
     function __construct()
     {
@@ -11,8 +11,8 @@ class Model
         Model::session_init();
 
         $attr = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-        self::$db = new PDO('mysql:host=' . SERVER_NAME . ';dbname=' . DATABASE, USERNAME, PASSWORD, $attr);
-        self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        self::$conn = new PDO('mysql:host=' . SERVER_NAME . ';dbname=' . DATABASE, USERNAME, PASSWORD, $attr);
+        self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         if (function_exists('jdate') == FALSE) {
             require('public/library/jdf/jdf.php');
         }
